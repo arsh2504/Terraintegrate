d92bf2bf1c8afe2ac10e8ff86f05c914037e726c
arsh2504 <143897789+arsh2504@users.noreply.github.com>
2023-11-20 21:24:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+
+  tags = {
+    Name = "HelloWorld, Terraform cloud"
+    
+  }
+}
